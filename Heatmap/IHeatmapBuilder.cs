55caa11c511827d9d553d343f2a97c17f36928fe
Tomasz Czarnecki <creo.one@gmail.com>
2022-06-13 19:32:35 +0200

    Changed code and docs to use IHeatmapBuilder

--- Heatmap/IHeatmapBuilder.cs
+++ Heatmap/IHeatmapBuilder.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Heatmap
 {
-    public interface IDefaultHeatmapBuilder
+    public interface IHeatmapBuilder
     {
         Task GenerateAsync();
         DefaultHeatmapBuilder SetGradient(IGradient gradient);
