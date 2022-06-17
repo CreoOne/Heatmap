996d0d7bd1f12585632ee7b7fae81d2e801e0039
Tomasz Czarnecki <creo.one@gmail.com>
2022-06-17 17:45:16 +0200

    Added clamp extensions benchmarks

---
+++ sources/Heatmap.Benchmarks/ClampingBenchmarks.cs
@@ -0,0 +1,17 @@
+﻿using BenchmarkDotNet.Attributes;
+using Heatmap.Extensions;
+
+namespace Heatmap.Benchmarks
+{
+    public class ClampingBenchmarks
+    {
+        [Params(-1d, 0d, 0.5d, 1d, Math.PI)]
+        public double Value { get; set; }
+
+        [Benchmark]
+        public double MathBased() => Math.Max(0, Math.Min(Value, 1));
+
+        [Benchmark]
+        public double ExtensionBased() => Value.Clamp(0, 1);
+    }
+}
